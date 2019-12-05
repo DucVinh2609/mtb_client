8a06b0dfb4e14bbb1d8f9613ad580b3cb8e24572
thien nguyen duc <tducnguyen1997@gmail.com>
2019-12-05 17:05:40 +0700

    schedule time movies 2

--- src/components/movie/TimeMovie.vue
+++ src/components/movie/TimeMovie.vue
@@ -5,13 +5,19 @@
         <p class="time-select__place">{{ moviesDate.showtime }}</p>
       </div>
 
-      <div v-for="moviesTime in moviesTimes" :key="moviesTime.id">
-        <p class="col-sm-8 items-wrap">
-          <div v-if="moviesTime.showtime === moviesDate.showtime">
-            <span class="time-select__item" >{{ moviesDate.time }}</span>
-          </div>
-        </p>
-      </div>
+      <!-- <div v-for="moviesTime in moviesTimes" :key="moviesTime.id"> -->
+        <ul class="col-sm-8 items-wrap">
+          <li v-for="moviesTime in moviesTimes" :key="moviesTime.id">
+            <div v-if="moviesTime.showtime === moviesDate.showtime">
+              <span class="time-select__item" >{{ moviesDate.time }}</span>
+            </div>
+          </li>
+
+          <!-- <div v-else>
+            <span class="time-select__item" hidden></span>
+          </div> -->
+        </ul>
+      <!-- </div> -->
     </div>
   </div>
 </template>
